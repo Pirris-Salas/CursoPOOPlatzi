8b983776448ecff1771a64dc77a76998c6be83c5
Pirris Salas <pirrissalas_10@hotmail.com>
2020-04-16 19:00:21 -0600

    Se declara un segundo metodo constructor en la clase Car.Java, además de que se genera una instancia para reducir lineas de código

--- Java/Main.java
+++ Java/Main.java
@@ -3,16 +3,16 @@ class Main {
 public static void main(String[] args) {
     
 //System.out.print("Hola Mundo ");
-Car car = new Car();
-car.license = "AMQ123";
-car.driver = "Andres Herrera";
+Car car = new Car("AMQ123", new Account("Andres Herrera", "And123"));
+//car.license = "AMQ123";
+//car.driver = "Andres Herrera";
 car.passenger = 4;
 //System.out.println("Car License: " + car.license);
 car.printDataCar();
 
-Car car2 = new Car();
-car2.license = "QWE567";
-car2.driver = "Andrea Herrera";
+Car car2 = new Car("QWE567", new Account("Andrea Herrera", "Andrea1234"));
+//car2.license = "QWE567";
+//car2.driver = "Andrea Herrera";
 car2.passenger = 3;
 
 //System.out.println("Car License: " + car2.license);
